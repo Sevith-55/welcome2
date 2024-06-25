ada899fe2a2b49e671ce46b138942ba355b853af
Sevith-55 <sevith.sadashiva@bca.christuniversity.in>
2024-06-25 17:25:05 +0530

    final

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import React from 'react'
+import Confetti from 'react-confetti'
+
+
+
+function App() {
+  return (
+    <>
+          <h1>Welcome Freshers!</h1>
+          <div className="card">
+
+            <Confetti
+              width={screen.width}
+              height={screen.height}
+              initialVelocityY={10}
+              initialVelocityX={5}
+              gravity={0.05}
+            />k
+
+
+          </div>
+    
+    </>
+  )
+}
+
+export default App
